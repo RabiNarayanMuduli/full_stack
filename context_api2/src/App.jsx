1c98d07b6717bd69b010ef16070a9916e1dfeb64
Rabi Narayan Muduli <rabinarayanmuduli@gmail.com>
2026-06-27 14:42:59 +0530

    hii

---
+++ context_api2/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className='text-3xl text-center my-3 underline'>This is my app</h1>
+    
+            <div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        
+                    </div>
+
+                    <div className="w-full max-w-sm mx-auto">
+                      
+                    </div>
+                </div>
+            </div>
+
+    </>
+  )
+}
+
+export default App
